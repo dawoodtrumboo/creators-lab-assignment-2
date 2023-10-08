f82efcbcef00f9e314f5de9e1698e2866e91763d
Dawood Trumboo <107537057+dawoodtrumboo@users.noreply.github.com>
2023-10-08 11:32:56 +0530

    assigment done

---
+++ src/Components/NavHoverBox.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { service1 } from '../assets'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+const NavHoverBox = ({category}) => {
+ 
+  return (
+    <div className='hidden  sm:block w-[80%] bg-[#0E0E30] absolute z-30 left-[150px] border-b-[5px] border-[#3846C5]'>
+    <div className='sm:flex flex-wrap px-10 py-[80px] gap-y-10'>
+        {category.items.map((item,index)=>(
+             <div className='card flex w-1/3 gap-5 items-start' key={index}>
+             {item.image? <div className='w-[150px]'>
+               <img src={item.image} alt='img' className='w-full h-full -mt-2'/>
+             </div>:''}
+            
+             <div className='space-y-2'>
+                <div>
+               <h1 className='font-bold text-[17px]'>{item.title} 
+               <FontAwesomeIcon icon={faChevronRight} size='xs' className='ml-2 mt-[3px]'/>
+                </h1>
+               
+               </div>
+               <p className='text-sm'>{item.description}</p>
+             </div>
+           </div>
+        ))}
+   
+
+    </div>
+  </div>
+  )
+}
+
+export default NavHoverBox
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import CustomButton from './CustomButton'
+import NavHoverBox from './NavHoverBox'
+import navdata from '../constants/navdata.json'
+
 
 const Navbar = () => {
+
+  const [navHover, setNavHover] = useState('');
+
+  const handleMenuItemHover = (itemName) => {
+    setNavHover(itemName);   
+  };
+
+  const handleMenuItemHoverOut = () => {
+    setNavHover('');
+  };
+
+  const category = navdata.categories.find(
+    (category) => category.name === navHover
+  );
+
+
   return (
+    <div>
     <div className='sticky top-0 md:px-[50px] text-white z-30 bg-[hsla(240,59%,7%,.35)] h-[100px]  backdrop-blur-[5px] flex items-center justify-between px-[15px]'>
  
         <div className='logo w-[170px]'>
@@ -16,19 +36,32 @@ const Navbar = () => {
             </div>
         </div>
         <div className='font-semibold sm:flex gap-[35px] hidden '>
-          <div>
+          <div
+          onMouseEnter={() => handleMenuItemHover('Services')}
+          onMouseLeave={handleMenuItemHoverOut}
+          className='cursor-pointer'
+          >
             <span>Services</span>
           </div>
-          <div>
+          <div
+          className='cursor-pointer'>
             <span>Managed Secuirity Services</span>
           </div>
-          <div>
+          <div
+          onMouseEnter={() => handleMenuItemHover('Initiatives')}
+          onMouseLeave={handleMenuItemHoverOut}
+          className='cursor-pointer'>
             <span>Initiatives</span>
           </div>
-          <div>
+          <div
+          onMouseEnter={() => handleMenuItemHover('About Us')}
+          onMouseLeave={handleMenuItemHoverOut}
+          className='cursor-pointer'
+          >
             <span>About Us</span>
           </div>
-          <div>
+          <div
+          className='cursor-pointer'>
             <span>Blog</span>
           </div>
         </div>
@@ -38,6 +71,8 @@ const Navbar = () => {
         <div className='hidden sm:block'>
             <CustomButton name="Contact Us" />
         </div>
+    </div>
+    {category?<NavHoverBox category = {category}/>:''} 
     </div>
   )
 }
